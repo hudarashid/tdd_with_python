1685abbdb07e61430fb28fe4615f3f1c33f545d3
hudarashid <hudarashid87@gmail.com>
2023-02-05 19:03:46 +0000

    comment out first

--- functional_tests.py
+++ functional_tests.py
@@ -1,12 +1,12 @@
-from selenium import webdriver
-from selenium.webdriver import FirefoxOptions
+# from selenium import webdriver
+# from selenium.webdriver import FirefoxOptions
 
-browser = webdriver.Chrome()
-#opts.add_argument("--headless")
-#browser = webdriver.ChromeOptions()
+# browser = webdriver.Chrome()
+# #opts.add_argument("--headless")
+# #browser = webdriver.ChromeOptions()
 
-print("BROWSER", browser)
+# print("BROWSER", browser)
 
-browser.get('http://127.0.0.1:8000')
+# browser.get('http://127.0.0.1:8000')
 
-assert 'Django' in browser.title
+# assert 'Django' in browser.title
